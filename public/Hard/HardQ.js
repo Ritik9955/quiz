32111f96c09cab70bf9b6e493401f7db8f129f62
Ritik9955 <42289891+Ritik9955@users.noreply.github.com>
2021-03-26 13:52:26 +0530

    a

---
+++ public/Hard/HardQ.js
@@ -0,0 +1,60 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: " JavaScript Code can be called by using ___________",
+    answer: "Function/Method",
+    options: [
+      "RMI",
+      "Triggering Event",
+      "Preprocessor",
+      "Function/Method"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The type of a variable that is volatile is ________",
+    answer: "Mutable variable",
+    options: [
+      "Volatile variable",
+      "Mutable variable",
+      "Immutable variablet",
+      "Dynamic variable"
+    ]
+  },
+    {
+    numb: 3,
+    question: "A hexadecimal literal begins with __________",
+    answer: "0x",
+    options: [
+      "0x",
+      "00",
+      "01",
+      "11"
+    ]
+  },
+    {
+    numb: 4,
+    question: " JavaScript _________ when there is an indefinite or an infinite value during an arithmetic computation.",
+    answer: "Displays “Infinity”",
+    options: [
+      "Prints an exception error",
+      "Prints an overflow error",
+      "Statement Question Language",
+      "Displays “Infinity”"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is not considered as an error in JavaScript?",
+    answer: "Division by zero",
+    options: [
+      "Syntax error",
+      " Missing of semicolons",
+      "Division by zero",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+];
+
---
+++ public/Medium/MedQue.js
@@ -0,0 +1,58 @@
+let Easyquestion = [
+    {
+    numb: 1,
+    question: "How can you access the state of a component from inside of a member function?",
+    answer: "this.values",
+    options: [
+      " this.getState()",
+      " this.prototype.stateValue",
+      "this.state",
+      "this.values"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Props are __________ into other components",
+    answer: "Injected",
+    options: [
+      "Injected",
+      "Render",
+      "The library of react is too larget",
+      "All of these"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following API is a MUST for every ReactJS component?",
+    answer: "renderComponent",
+    options: [
+      "getInitialState",
+      "render",
+      "renderComponent",
+      "None"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is a react.js in MVC?",
+    answer: " Controller",
+    options: [
+      " Controller",
+      "Model",
+      " Router",
+      "Middleware"
+    ]
+  },
+    {
+    numb: 5,
+    question: " At the highest level, React components have lifecycle events that fall into",
+    answer: "All of these",
+    options: [
+      " Initialization",
+      " State/Property Updates",
+      "Destructions",
+      "All of these"
+    ]
+  },
+  
+  ];
